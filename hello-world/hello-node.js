69dc18a000648adece92d042e58ffa6d1ad8534b
Filipp Trigub (Blackbox Agent) <filipp.trigub@gmail.com>
2026-01-05 13:36:51 +0000

    feat(widget): add hello world example scripts

---
+++ hello-world/hello-node.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+// Node.js Hello World (can be run as a script)
+const message = "Hello, World!";
+console.log(message);
+
+// Express web server example
+// Uncomment below to use with Node.js web server
+/*
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!\n');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
+*/
