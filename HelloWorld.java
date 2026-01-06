6ad2f75eae3fab5a8d7b0ee418b1d94bb5a9b098
Filipp Trigub (Blackbox Agent) <filipp.trigub@gmail.com>
2026-01-06 13:56:21 +0000

    I'm ready to generate a commit message, but I notice the task descrip...

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("Starting a loop...");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+            if (i % 2 == 0) {
+                System.out.println(i + " is even.");
+            } else {
+                System.out.println(i + " is odd.");
+            }
+        }
+        System.out.println("Loop finished.");
     }
 }
