614ac24650a4425116e5ad6c9fadc29cca6086f2
Filipp Trigub (Blackbox Agent) <filipp.trigub@gmail.com>
2026-02-26 22:57:21 +0000

    feat(ui): add centered red circle and FOOBAR/NEXT_PUBLIC_FOOBAR banner

--- server.js
+++ server.js
@@ -9,28 +9,11 @@ app.use(express.static(path.join(__dirname)));
 // Check for environment variables
 const foobar = process.env.FOOBAR || process.env.NEXT_PUBLIC_FOOBAR;
 
-// Function to add banner to HTML content
+// Function to add banner and red circle to HTML content
 function addBannerToHtml(content, isHeaderPage = false) {
   if (foobar) {
-    const bannerHtml = `
-      <div id="env-banner" style="background-color: #ff6b6b; color: white; text-align: center; padding: 10px; font-weight: bold; position: fixed; top: 0; left: 0; width: 100%; z-index: 1001;">
-        Environment Variable Set: ${foobar}
-      </div>
-      <style>
-        ${isHeaderPage ? 
-          `header.enhanced-header {
-          top: 40px !important;
-        }
-        .page-breadcrumb {
-          top: 40px !important;
-        }` : 
-          `header {
-          top: 40px !important;
-        }`
-        }
-      </style>
-    `;
-    content = content.replace('</head>', `${bannerHtml}</head>`);
+    const bannerHtml = '<div id="env-banner" style="background-color: #ff6b6b; color: white; text-align: center; padding: 10px; font-weight: bold; position: fixed; top: 0; left: 0; width: 100%; z-index: 1001;">Environment Variable Set: ' + foobar + '</div><div id="red-circle" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 200px; height: 200px; background-color: red; border-radius: 50%; z-index: 999;"></div><style>header { top: 40px !important; }.page-breadcrumb { top: 40px !important; }</style>';
+    content = content.replace('</head>', bannerHtml + '</head>');
   }
   return content;
 }
@@ -40,10 +23,21 @@ app.get('/', (req, res) => {
   // Read the header.html file
   const fs = require('fs');
   let content = fs.readFileSync(path.join(__dirname, 'header.html'), 'utf8');
-  
+
   // Add banner if environment variable is set
   content = addBannerToHtml(content, true);
-  
+
+  res.send(content);
+});
+
+// Route for index.html redirect - handle properly
+app.get('/index.html', (req, res) => {
+  const fs = require('fs');
+  let content = fs.readFileSync(path.join(__dirname, 'header.html'), 'utf8');
+
+  // Add banner if environment variable is set
+  content = addBannerToHtml(content, true);
+
   res.send(content);
 });
 
