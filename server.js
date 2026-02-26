e0ef52b19d7a2fadc47fae27566ec48d43308569
Filipp Trigub (Blackbox Agent) <filipp.trigub@gmail.com>
2026-02-26 20:51:18 +0000

    feat(ui): add center red circle and FOOBAR/NEXT_PUBLIC banner

--- server.js
+++ server.js
@@ -3,35 +3,51 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
-app.use(express.static(path.join(__dirname)));
+// Serve static files but exclude HTML files so routes can handle them
+app.use((req, res, next) => {
+  // Skip static serving for HTML files and redirect them to route handlers
+  if (req.path.endsWith('.html')) {
+    return next();
+  }
+  express.static(path.join(__dirname))(req, res, next);
+});
 
 // Check for environment variables
 const foobar = process.env.FOOBAR || process.env.NEXT_PUBLIC_FOOBAR;
 
-// Function to add banner to HTML content
+// Function to add banner and red circle to HTML content
 function addBannerToHtml(content, isHeaderPage = false) {
+  let additions = '';
+
   if (foobar) {
-    const bannerHtml = `
+    additions += `
       <div id="env-banner" style="background-color: #ff6b6b; color: white; text-align: center; padding: 10px; font-weight: bold; position: fixed; top: 0; left: 0; width: 100%; z-index: 1001;">
         Environment Variable Set: ${foobar}
-      </div>
+      </div>`;
+  }
+
+  // Add the red circle
+  additions += `
+    <div id="red-circle" style="position: fixed; width: 200px; height: 200px; background-color: red; border-radius: 50%; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 999;"></div>`;
+
+  if (foobar) {
+    additions += `
       <style>
-        ${isHeaderPage ? 
+        ${isHeaderPage ?
           `header.enhanced-header {
           top: 40px !important;
         }
         .page-breadcrumb {
           top: 40px !important;
-        }` : 
+        }` :
           `header {
           top: 40px !important;
         }`
         }
-      </style>
-    `;
-    content = content.replace('</head>', `${bannerHtml}</head>`);
+      </style>`;
   }
+
+  content = content.replace('</head>', `${additions}</head>`);
   return content;
 }
 
@@ -40,10 +56,21 @@ app.get('/', (req, res) => {
   // Read the header.html file
   const fs = require('fs');
   let content = fs.readFileSync(path.join(__dirname, 'header.html'), 'utf8');
-  
+
   // Add banner if environment variable is set
   content = addBannerToHtml(content, true);
-  
+
+  res.send(content);
+});
+
+// Route for header.html
+app.get('/header.html', (req, res) => {
+  const fs = require('fs');
+  let content = fs.readFileSync(path.join(__dirname, 'header.html'), 'utf8');
+
+  // Add banner if environment variable is set
+  content = addBannerToHtml(content, true);
+
   res.send(content);
 });
 
@@ -51,10 +78,10 @@ app.get('/', (req, res) => {
 app.get('/team.html', (req, res) => {
   const fs = require('fs');
   let content = fs.readFileSync(path.join(__dirname, 'team.html'), 'utf8');
-  
+
   // Add banner if environment variable is set
   content = addBannerToHtml(content, false);
-  
+
   res.send(content);
 });
 
