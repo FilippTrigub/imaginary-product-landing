f992960b6a6cdb9d55d071752df7ea427a08bb9a
Filipp Trigub (Blackbox Agent) <filipp.trigub@gmail.com>
2026-02-27 00:01:39 +0000

    feat(ui): add centered red circle and FOOBAR/NEXT_PUBLIC banner

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
-app.use(express.static(path.join(__dirname)));
+// Serve static files (but don't serve index.html automatically for /)
+app.use(express.static(path.join(__dirname), {
+  index: false
+}));
 
 // Check for environment variables
 const foobar = process.env.FOOBAR || process.env.NEXT_PUBLIC_FOOBAR;
+console.log('Environment variables - FOOBAR:', process.env.FOOBAR, 'NEXT_PUBLIC_FOOBAR:', process.env.NEXT_PUBLIC_FOOBAR, 'Final foobar:', foobar);
 
 // Function to add banner to HTML content
 function addBannerToHtml(content, isHeaderPage = false) {
@@ -17,15 +20,21 @@ function addBannerToHtml(content, isHeaderPage = false) {
         Environment Variable Set: ${foobar}
       </div>
       <style>
-        ${isHeaderPage ? 
+        ${isHeaderPage ?
           `header.enhanced-header {
           top: 40px !important;
         }
         .page-breadcrumb {
           top: 40px !important;
-        }` : 
+        }
+        .red-circle {
+          top: calc(50% + 20px) !important;
+        }` :
           `header {
           top: 40px !important;
+        }
+        .red-circle {
+          top: calc(50% + 20px) !important;
         }`
         }
       </style>
@@ -40,10 +49,24 @@ app.get('/', (req, res) => {
   // Read the header.html file
   const fs = require('fs');
   let content = fs.readFileSync(path.join(__dirname, 'header.html'), 'utf8');
-  
+
+  // Add banner if environment variable is set
+  console.log('Processing / route, foobar:', foobar);
+  content = addBannerToHtml(content, true);
+  console.log('Content after adding banner includes env-banner:', content.includes('env-banner'));
+
+  res.send(content);
+});
+
+// Route for header.html directly
+app.get('/header.html', (req, res) => {
+  // Read the header.html file
+  const fs = require('fs');
+  let content = fs.readFileSync(path.join(__dirname, 'header.html'), 'utf8');
+
   // Add banner if environment variable is set
   content = addBannerToHtml(content, true);
-  
+
   res.send(content);
 });
 
